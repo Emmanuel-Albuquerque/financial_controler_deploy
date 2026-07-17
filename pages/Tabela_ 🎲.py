0402e30cf25d7413cc02c2f30f700056a681a1ca
Emmanuel-Albuquerque <emanoag@gmail.com>
2026-07-17 13:56:41 -0300

    Update Tabela_ 🎲.py

--- pages/Tabela_ 🎲.py
+++ pages/Tabela_ 🎲.py
@@ -1,10 +1,16 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
+from datetime import datetime
 
 # conexão planilha
+
+hoje = datetime.now()
+ano = str(hoje.year)[2:]
+mes = str(hoje.month)
+
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-df = conn.read(worksheet="Página1", ttl=0)
+df = conn.read(worksheet=f"{mes}/{ano}", ttl=0)
 
 st.title('Segue a base de dados atual 💸')
 
